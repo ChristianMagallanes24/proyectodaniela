89fce2e234eac3f119f23db317ab131f96016c35
Sofía Navas <133719420+sofinavas@users.noreply.github.com>
2024-01-21 19:15:45 -0300

    cambios

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import "./App.scss";
-import Header from "./components/Header/Header";
-import Navbar from "./components/NavBar/NavBar";
+
 import Footer from "./components/Footer/footer";
 
 /* The following line can be included in your src/index.js or App.js file */
@@ -9,8 +8,6 @@ import "./App.scss";
 function App() {
   return (
     <div className="App">
-      <Navbar />
-      <Header />
       <Footer />
     </div>
   );
