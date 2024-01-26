cd5c632c021b30bd090d2f6bdc1889fd94b06038
Sofía Navas <133719420+sofinavas@users.noreply.github.com>
2024-01-26 17:30:00 -0300

    scroll hacia arriba

--- src/components/SobreNosotros/SobreNosotros.jsx
+++ src/components/SobreNosotros/SobreNosotros.jsx
@@ -1,76 +1,67 @@
-import React from "react";
+import React, { useEffect } from "react";
+
 import "../SobreNosotros/SobreNosotros.scss";
 import Footer from "../Footer/footer";
 
 const SobreNosotros = () => {
+  useEffect(() => {
+    window.scrollTo(0, 0); // Hacer scroll al principio de la página al cargar el componente
+  }, []);
+
   return (
-
     <div className="sobre-nosotros-container">
-    <div className="sobre-nosotros">
-      {/* sobre nosotros "pagina" */}
-      <h1 className="descripcion-titulo">Sobre nosotros</h1>
-      <div className="descripcion">
-        
-        <p className="descripcion-parrafo">
-          Somos una organización virtual que se expande a nivel global
-          ofreciendo servicios de capital humano para empresas. Nos respaldan 14
-          años en el sector de capital humano dedicados a brindar servicios
-          profesionales. Principalmente proveemos perfiles de colaboradores para
-          aumentar el valor de tus experiencias como soluciones para mejorar tus
-          estrategias en un marco de agilidad y disrupción.
-        </p>
+      <div className="sobre-nosotros">
+        {/* sobre nosotros "pagina" */}
+        <h1 className="descripcion-titulo">Sobre nosotros</h1>
+        <div className="descripcion">
+          <p className="descripcion-parrafo">
+            Somos una organización virtual que se expande a nivel global
+            ofreciendo servicios de capital humano para empresas. Nos respaldan
+            14 años en el sector de capital humano dedicados a brindar servicios
+            profesionales. Principalmente proveemos perfiles de colaboradores
+            para aumentar el valor de tus experiencias como soluciones para
+            mejorar tus estrategias en un marco de agilidad y disrupción.
+          </p>
+        </div>
       </div>
-      </div>
-    
-    
-     
+
       <div className="cuadros">
-
         <div className="cuadro">
-        <h1 className="cuadro-titulo">Misión</h1>
+          <h1 className="cuadro-titulo">Misión</h1>
           <div className="cuadro-info">
-         
-          <p className="cuadro-parrafo">
-            Brindar un servicio integral, personalizado y de calidad en el
-            ámbito de capital humano, orientado a satisfacer tus necesidades y
-            expectativas a través de soluciones que aporten valor a tus
-            proyectos
-          </p>
+            <p className="cuadro-parrafo">
+              Brindar un servicio integral, personalizado y de calidad en el
+              ámbito de capital humano, orientado a satisfacer tus necesidades y
+              expectativas a través de soluciones que aporten valor a tus
+              proyectos
+            </p>
           </div>
-
         </div>
 
         <div className="cuadro">
-        <h1 className="cuadro-titulo">Visión</h1>
+          <h1 className="cuadro-titulo">Visión</h1>
           <div className="cuadro-info">
-          
-          <p className="cuadro-parrafo">
-            Reconocimiento multiculturalmente por nuestra capacidad de apoyar
-            tus necesidades de capital humano, contribuyendo al éxito
-            organizacional y al bienestar de las personas.
-          </p>
+            <p className="cuadro-parrafo">
+              Reconocimiento multiculturalmente por nuestra capacidad de apoyar
+              tus necesidades de capital humano, contribuyendo al éxito
+              organizacional y al bienestar de las personas.
+            </p>
           </div>
-
         </div>
 
         <div className="cuadro">
-        <h1 className="cuadro-titulo">Valores</h1>
+          <h1 className="cuadro-titulo">Valores</h1>
           <div className="cuadro-info">
-          
-          <p className="cuadro-parrafo">
-            Basados en la colaboración, el enfoque, la profesionalidad  y la
-            responsabilidad.
-          </p>
+            <p className="cuadro-parrafo">
+              Basados en la colaboración, el enfoque, la profesionalidad  y la
+              responsabilidad.
+            </p>
           </div>
-
         </div>
-
       </div>
-      
-   
-    <Footer />
+
+      <Footer />
     </div>
-    
   );
 };
 
